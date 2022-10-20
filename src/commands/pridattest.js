ff631cc35c7aa5a91fd4159538ded73080c357d6
Martin97y <martinbotko97@gmail.com>
2022-10-20 18:05:09 +0200

    done

--- src/commands/pridattest.js
+++ src/commands/pridattest.js
@@ -3,6 +3,7 @@ import { SlashCommandBuilder } from '@discordjs/builders';
  const pridatTesty = new SlashCommandBuilder()
  .setName('pridat-test')
  .setDescription('Přidá test.')
+ .setDefaultMemberPermissions(PermissionFlagsBits.PrioritySpeaker)
  .addStringOption((option) => option.setName('popis').setDescription('Co se bude psát.').setRequired(true))
  .addIntegerOption((option) => option.setName('kdy').setDescription('Datum kdy.').setRequired(true))
 
--- src/commands/pridatukol.js
+++ src/commands/pridatukol.js
@@ -1,8 +1,10 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { PermissionFlagsBits } from 'discord.js';
 
     const pridatUkoly = new SlashCommandBuilder()
     .setName('pridatukol')
     .setDescription('Přidá úkol.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.PrioritySpeaker)
     .addStringOption((option) => option.setName('popis').setDescription('Strana. atd').setRequired(true))
     .addIntegerOption((option) => option.setName('dokdy').setDescription('Datum do kdy.').setRequired(true))
 
