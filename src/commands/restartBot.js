214f6018203127928fbc56597980c891b96e87cb
Martin97y <martinbotko97@gmail.com>
2023-05-30 16:34:12 +0200

    Update

--- src/commands/restartBot.js
+++ src/commands/restartBot.js
@@ -4,6 +4,6 @@ import { PermissionFlagsBits } from 'discord.js';
  const restartBot = new SlashCommandBuilder()
  .setName('restart')
  .setDescription('Restartuje bota.')
- .setDefaultMemberPermissions(PermissionFlagsBits.PrioritySpeaker)
+ .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 
 export default restartBot.toJSON();
