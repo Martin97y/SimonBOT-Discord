95947fb55a0307c1ef291fa7db8dc8bfab94209f
Martin97y <80329375+Martin97y@users.noreply.github.com>
2023-07-07 17:40:37 +0200

    Delete tests directory

--- tests/response.test.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'; 
-describe("Check DB", () => {
-    it("should connect", async () => {
-        const connect = await mongoose.connect("mongodb+srv://msOS:UGWW7Mys3Cxih4tF@accountmanager.b2vkp4o.mongodb.net/");
-        expect(mongoose.connection.readyState).toBe(1);
-        connect.connection.close();
-    });
-});
